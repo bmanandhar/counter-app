84e6cd98109e394da9ca3558425439f54446113e
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-03-28 09:46:05 -0700

    adds image attribute

--- README.md
+++ README.md
@@ -66,3 +66,6 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+https://www.youtube.com/watch?v=Ke90Tje7VS0
+https://picsum.photos
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 class Counter extends Component {
-  state = { 
-    count: 0
+  state = {
+    count: 0,
+    imageURL: 'https://picsum.photos/200'
    }
   render() { 
     return (
       <React.Fragment>
-        <span>{this.state.count}</span>
+        <img src={this.state.imageURL} alt=""/>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
     );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
   }
 }
  
