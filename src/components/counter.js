8dc209450dd69623e59d81bd088fca4fe810b2bb
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-03-28 10:00:18 -0700

    adds styles property to state

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 class Counter extends Component {
   state = {
     count: 0
-   }
+   };
+
+  styles = {
+    fontSize: 50,
+    fontWeight: 'bold'
+  }
   render() { 
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
