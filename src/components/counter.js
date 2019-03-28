7f71ba6e425d7ebed88b8f41f7a517cc247ecbb7
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-03-28 10:10:39 -0700

    adds className as required

--- src/components/counter.js
+++ src/components/counter.js
@@ -5,9 +5,11 @@ class Counter extends Component {
    };
 
   render() { 
+    let classes="badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
     return (
       <React.Fragment>
-        <span style={{fontSize: 30}} className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span style={{ fontSize: 20 }} className={classes}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
