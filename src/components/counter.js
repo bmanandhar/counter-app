1f84ab6813d4a28fb9030d46e053c3c2852b0f8e
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-03-28 10:20:35 -0700

    adds new method getBadgeClasses() as a separate func

--- src/components/counter.js
+++ src/components/counter.js
@@ -5,15 +5,19 @@ class Counter extends Component {
    };
 
   render() { 
-    let classes="badge m-2 badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
     return (
       <React.Fragment>
-        <span style={{ fontSize: 20 }} className={classes}>{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? 'Zero' : count;
